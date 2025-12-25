db2d35a44fca1bcc2ad34830fe446f5afa093513
carloshouser@gmail.com <carloshouser@gmail.com>
2025-12-25 13:35:11 -0300

    Teste

--- paginas/home.py
+++ paginas/home.py
@@ -3,7 +3,7 @@ import sessao_controle
 from pathlib import Path
 
 def render_home():
-    st.title("Flamboyant - Quadro de Anúncios")
+    st.title("Flamboyant")
 
     # Caminho seguro da imagem
     img_path = Path(__file__).parent.parent / "assets" / "imagens" / "salao.png"
